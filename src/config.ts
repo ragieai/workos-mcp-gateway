b1875d3311fea6a18f71920aa05d0a95b6fa338d
Bob Remeika <bob@ragie.ai>
2025-11-03 06:23:38 -0800

    Implement multi-tenant frontend by WorkOS organization ID
    * Remove endpoints that are not needed for MCP auth
    * Validate organization membership because it is not returned in
      the access token

--- src/config.ts
+++ src/config.ts
@@ -9,9 +9,6 @@ export interface GatewayConfig {
   workosApiKey: string;
   workosAuthorizationServerUrl: string;
   workosClientId: string;
-  workosCookiePassword: string;
-  workosRedirectUri: string;
-  workosOrganization: string;
 }
 
 export function getConfigFromEnv(): GatewayConfig {
@@ -24,9 +21,6 @@ export function getConfigFromEnv(): GatewayConfig {
     WORKOS_API_KEY: z.string(),
     WORKOS_AUTHORIZATION_SERVER_URL: z.string(),
     WORKOS_CLIENT_ID: z.string(),
-    WORKOS_COOKIE_PASSWORD: z.string(),
-    WORKOS_REDIRECT_URI: z.string(),
-    WORKOS_ORGANIZATION: z.string(),
   });
 
   const env = envVarSchema.parse(process.env);
@@ -40,8 +34,5 @@ export function getConfigFromEnv(): GatewayConfig {
     workosApiKey: env.WORKOS_API_KEY,
     workosAuthorizationServerUrl: env.WORKOS_AUTHORIZATION_SERVER_URL,
     workosClientId: env.WORKOS_CLIENT_ID,
-    workosCookiePassword: env.WORKOS_COOKIE_PASSWORD,
-    workosRedirectUri: env.WORKOS_REDIRECT_URI,
-    workosOrganization: env.WORKOS_ORGANIZATION,
   };
 }
